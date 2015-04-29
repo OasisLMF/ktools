12450192e25d21b3573c30bf8ab7c8bdc94b3af1
bmatharu <neb@sky.com>
2015-04-29 11:57:38 +0100

    Fix precision

--- src/outputcalc/outputcalc.cpp
+++ src/outputcalc/outputcalc.cpp
@@ -35,6 +35,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -179,7 +180,7 @@ void dofmsummary(std::map<output_key, std::vector<vecrec> > &output_map_, unsign
        float mean = sumloss / sample_size_;
        float sd = 0;
        if (sample_size_ > 1) sd = sqrt((sumlosssqr - ((sumloss*sumloss)/sample_size_))/(sample_size_ - 1));
-       cout << mean << "," << sd << "," << maxsumtiv << "\n";
+       cout << mean << "," << sd << "," << std::setprecision(9) << maxsumtiv << "\n";
 
        iter++;
     }
@@ -263,7 +264,7 @@ void dogulsummary(int event_id_,std::vector<vecrec> &output_vec_, unsigned int s
        float sd = 0;
        if (sample_size_ > 1) sd = sqrt((sumlosssqr - ((sumloss*sumloss)/sample_size_))/(sample_size_ - 1));
 
-       cout << mean << "," << sd << "," << maxsumtiv << "\n";
+       cout << mean << "," << sd << "," << std::setprecision(9) << maxsumtiv << "\n";
 
 
 }

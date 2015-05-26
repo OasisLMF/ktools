e0efa49cb62b982395151b0cfc089fabda2dcd2c
bmatharu <neb@sky.com>
2015-05-26 11:03:25 +0100

    Improved test script

--- ktest/runtests.sh
+++ ktest/runtests.sh
@@ -31,21 +31,11 @@ cd examples
 ../src/eve/eve 2 1 2 > ../ktest/testout/eveout2_1.bin
 ../src/eve/eve 2 2 2 > ../ktest/testout/eveout2_2.bin
 
-diff ../ktest/$CTRL/eveout1_1.bin ../ktest/testout/eveout1_1.bin
-diff ../ktest/$CTRL/eveout1_2.bin ../ktest/testout/eveout1_2.bin
-diff ../ktest/$CTRL/eveout2_1.bin ../ktest/testout/eveout2_1.bin
-diff ../ktest/$CTRL/eveout2_2.bin ../ktest/testout/eveout2_2.bin
-
 # test getmodel
 ../src/eve/eve 1 1 2 | ../src/getmodel/getmodel 1  > ../ktest/testout/getmodelout1_1.bin
 ../src/eve/eve 1 2 2 | ../src/getmodel/getmodel 1  > ../ktest/testout/getmodelout1_2.bin
 ../src/eve/eve 2 1 2 | ../src/getmodel/getmodel 2  > ../ktest/testout/getmodelout2_1.bin
 ../src/eve/eve 2 2 2 | ../src/getmodel/getmodel 2  > ../ktest/testout/getmodelout2_2.bin
-
-diff ../ktest/$CTRL/getmodelout1_1.bin ../ktest/testout/getmodelout1_1.bin
-diff ../ktest/$CTRL/getmodelout1_2.bin ../ktest/testout/getmodelout1_2.bin
-diff ../ktest/$CTRL/getmodelout2_1.bin ../ktest/testout/getmodelout2_1.bin
-diff ../ktest/$CTRL/getmodelout2_2.bin ../ktest/testout/getmodelout2_2.bin
 
 # test gulcalc
 ../src/eve/eve 1 1 2 | ../src/getmodel/getmodel 1 | ../src/gulcalc/gulcalc -C1 -S100 -R -r > ../ktest/testout/gulcalcout1_1.bin
@@ -53,48 +43,40 @@ diff ../ktest/$CTRL/getmodelout2_2.bin ../ktest/testout/getmodelout2_2.bin
 ../src/eve/eve 2 1 2 | ../src/getmodel/getmodel 2 | ../src/gulcalc/gulcalc -C1 -S100 -R -r > ../ktest/testout/gulcalcout2_1.bin
 ../src/eve/eve 2 2 2 | ../src/getmodel/getmodel 2 | ../src/gulcalc/gulcalc -C1 -S100 -R -r > ../ktest/testout/gulcalcout2_2.bin
 
-diff ../ktest/$CTRL/gulcalcout1_1.bin ../ktest/testout/gulcalcout1_1.bin
-diff ../ktest/$CTRL/gulcalcout1_2.bin ../ktest/testout/gulcalcout1_2.bin
-diff ../ktest/$CTRL/gulcalcout2_1.bin ../ktest/testout/gulcalcout2_1.bin
-diff ../ktest/$CTRL/gulcalcout2_2.bin ../ktest/testout/gulcalcout2_2.bin
-
 # test fmcalc
 ../src/eve/eve 1 1 2 | ../src/getmodel/getmodel 1 | ../src/gulcalc/gulcalc -C1 -S100 -R -r | ../src/fmcalc/fmcalc >  ../ktest/testout/fmcalcout1_1.bin
-
-diff ../ktest/$CTRL/fmcalcout1_1.bin ../ktest/testout/fmcalcout1_1.bin
 
 # test outputcalc
 ../src/eve/eve 1 1 1 | ../src/getmodel/getmodel 1 | ../src/gulcalc/gulcalc -C1 -S100 -R -r | ../src/outputcalc/outputcalc >  ../ktest/testout/gulout1.csv
 
-diff ../ktest/$CTRL/gulout1.csv ../ktest/testout/gulout1.csv
-
 ../src/eve/eve 1 1 1 | ../src/getmodel/getmodel 1 | ../src/gulcalc/gulcalc -C1 -S100 -R -r | ../src/fmcalc/fmcalc | ../src/outputcalc/outputcalc >  ../ktest/testout/fmout1.csv
-
-diff ../ktest/$CTRL/fmout1.csv ../ktest/testout/fmout1.csv
 
 # test data conversion utilities
 # stdout to csv
 ../src/cdftocsv/cdftocsv < ../ktest/testout/getmodelout1_1.bin > ../ktest/testout/getmodelout1_1.csv
 
-diff ../ktest/$CTRL/getmodelout1_1.csv ../ktest/testout/getmodelout1_1.csv
-
 ../src/gultocsv/gultocsv < ../ktest/testout/gulcalcout1_1.bin > ../ktest/testout/gulcalcout1_1.csv
 
-diff ../ktest/$CTRL/gulcalcout1_1.csv ../ktest/testout/gulcalcout1_1.csv
-
 ../src/fmtocsv/fmtocsv < ../ktest/testout/fmcalcout1_1.bin > ../ktest/testout/fmcalcout1_1.csv
-
-diff ../ktest/$CTRL/fmcalcout1_1.csv ../ktest/testout/fmcalcout1_1.csv
 
 # input data to csv and bin
 ../src/evetocsv/evetocsv < ../examples/e_chunk_1_data.bin > ../ktest/testout/e_chunk_1_data.csv
 ../src/evetobin/evetobin < ../ktest/testout/e_chunk_1_data.csv > ../ktest/testout/e_chunk_1_data.bin
 
-diff ../examples/e_chunk_1_data.bin ../ktest/testout/e_chunk_1_data.bin
-
 #../src/randtocsv/randtocsv < ../examples/random_1.bin > ../ktest/testout/random_1.csv
 #../src/randtobin/randtobin < ../ktest/testout/random_1.csv > ../ktest/testout/random_1.bin
 
 #diff ../examples/random_1.bin ../ktest/testout/random_1.bin
+
+cd ../ktest/testout
+md5sum -c ../$CTRL.md5
+
+if [ "$?" -ne "0" ]; then
+  echo "Sorry check failed\n"
+  exit 1
+else
+  echo "All tests passed.\n"
+ exit 0
+fi
 
 

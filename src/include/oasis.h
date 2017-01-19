8aa79309b60f8dacd60df46b81ed3befaa308176
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2017-01-19 12:12:42 +0000

    Moved occurrence.bin from static folder to input folder

--- src/include/oasis.h
+++ src/include/oasis.h
@@ -295,11 +295,11 @@ inline void initstreams(std::string inFile="", std::string outFile="")
 #define ITEMS_FILE "input/items.bin"
 #define EVENTS_FILE "input/events.bin"
 #define RETURNPERIODS_FILE "input/returnperiods.bin"
+#define OCCURRENCE_FILE "input/occurrence.bin"
 
 #define DAMAGE_BIN_DICT_FILE "static/damage_bin_dict.bin"
 #define DAMAGE_CDF_BIN_FILE "static/damage_cdf.bin"
 #define DAMAGE_CDF_IDX_FILE "static/damage_cdf.idx"
-#define OCCURRENCE_FILE "static/occurrence.bin"
 #define RANDOM_FILE "static/random.bin"
 #define VULNERABILITY_FILE  "static/vulnerability.bin"
 #define FOOTPRINT_FILE  "static/footprint.bin"

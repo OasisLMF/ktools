1595d9e2e191d635ddc3beb89eab044e23e8df07
Ben Matharu <neb@sky.com>
2018-07-17 16:08:09 +0100

    xaalcalc updated to compile

--- src/xaalcalc/main.cpp
+++ src/xaalcalc/main.cpp
@@ -1,2 +1,45 @@
-// todo
-//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aalcalc.h"
+#include "../../config.h"
+
+#if defined(_MSC_VER)
+#include "../wingetopt/wingetopt.h"
+#else
+#include <unistd.h>
+#endif
+
+
+
+void help()
+{
+	fprintf(stderr, "-h help\n-v version\n");
+}
+
+int main(int argc, char* argv[])
+{
+
+	int opt;
+	int processid = 0;
+	while ((opt = getopt(argc, argv, (char *)"vhP:")) != -1) {
+		switch (opt) {
+		case 'v':
+			fprintf(stderr, "%s : version: %s\n", argv[0], VERSION);
+			exit(EXIT_FAILURE);
+			break;
+		case 'h':
+		default:
+			help();
+			exit(EXIT_FAILURE);
+		}
+	}
+	initstreams();
+
+	aalcalc a;
+	a.doit();
+
+	return EXIT_SUCCESS;
+
+}

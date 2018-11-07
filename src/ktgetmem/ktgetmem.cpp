7ea443ea8521d051d70b310e13eacee9fa04455f
Ben Matharu <neb@sky.com>
2018-11-07 12:09:41 +0000

    added code for ktgetmem

--- src/ktgetmem/ktgetmem.cpp
+++ src/ktgetmem/ktgetmem.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void getcgroupLimit()
+{
+	char line[1024];
+	FILE *fin = fopen("/sys/fs/cgroup/memory/memory.limit_in_bytes","rb");
+	if (fin == NULL) {
+		fprintf(stderr,"Open failed");
+		return;
+	}
+	fgets(line, sizeof(line), fin);
+	printf(line);
+	fclose(fin);
+}
+
+
 int main()
 {
-
+	getcgroupLimit();
 }

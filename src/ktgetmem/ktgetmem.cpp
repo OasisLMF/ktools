6188b7deaa0d510a18f183e5f6f725b3b20f19df
Ben Matharu <neb@sky.com>
2018-11-08 10:40:08 +0000

    code to trap memory allocation errors before cgroup kills the processes

--- src/ktgetmem/ktgetmem.cpp
+++ src/ktgetmem/ktgetmem.cpp
@@ -1,20 +1,60 @@
 #include <stdio.h>
+#include <string>
 
-void getcgroupLimit()
+#include <sys/sysinfo.h>
+
+
+void getfreemem()
+{
+	struct sysinfo sys_info;
+    sysinfo(&sys_info);
+    sysinfo(&sys_info);
+    int memlimit = sys_info.freeram / 1024 * sys_info.mem_unit;
+    int slack = memlimit / 20;
+    memlimit = memlimit - slack;
+    printf ("%d\n",memlimit);
+    return;
+	
+}
+
+void getcgroupLimit(int core_count)
 {
 	char line[1024];
 	FILE *fin = fopen("/sys/fs/cgroup/memory/memory.limit_in_bytes","rb");
 	if (fin == NULL) {
 		fprintf(stderr,"Open failed");
 		return;
-	}
+	}	
 	fgets(line, sizeof(line), fin);
-	printf(line);
-	fclose(fin);
+
+	std::string s = line;
+	s.erase(s.length()-1);	// strip new line
+	if (s == "9223372036854771712") {
+		getfreemem();
+	}else {
+		long long int memlimit;
+		if (sscanf(line, "%lld", &memlimit) != 1){
+	           fprintf(stderr, "Invalid data : %s\n", line);
+	           return ;
+	    }
+
+	    fclose(fin);
+
+	    memlimit = memlimit / 1024;	   	// convert to kilobytes
+
+	    long long int slack = memlimit / 20;	// leave 5 percent of memory
+	    if (slack < 4096) {
+	    	slack =4096;
+	    }
+	    memlimit = memlimit - slack;		// remove 5 % from mem from limit	
+	    memlimit = memlimit / core_count;
+		printf("%lld",memlimit);		// ouput memory limit - 4 meg		
+	}
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	getcgroupLimit();
+	int core_count = atoi(argv[1]);
+	getcgroupLimit(core_count);	
 }

f04cc71888b759c14298df1beafadd2c4dd671e4
Joh <johanna.carter@oasislmf.org>
2020-01-10 13:50:13 +0000

    added calcrules 30 and 31 for step policies

--- src/fmcalc/fmcalcrules.cpp
+++ src/fmcalc/fmcalcrules.cpp
@@ -225,6 +225,104 @@ void applycalcrule_stepped(const profile_rec_new& profile, LossRec& x, int layer
 			}
 		}
 		break;
+		case 30:
+		{
+			OASIS_FLOAT tstart = 0;
+			OASIS_FLOAT tend = 0;
+			OASIS_FLOAT payout = 0;
+			OASIS_FLOAT scale1 = 0;
+			OASIS_FLOAT limit1 = 0;
+
+			for (auto y : profile.tc_vec) {
+				if (y.tc_id == trigger_start) tstart = y.tc_val;
+				if (y.tc_id == trigger_end) tend = y.tc_val;
+				if (y.tc_id == payout_start) payout = y.tc_val;
+				if (y.tc_id == scale_1) scale1 = y.tc_val;
+				if (y.tc_id == limit_1) limit1 = y.tc_val;
+			}
+			// Step policy: single step with % limit payout and an optional gross up factor for secondary coverages
+			OASIS_FLOAT loss = 0;
+
+			loss = x.loss / x.accumulated_tiv;
+			if (tend == 1) { // if the upper threshold is 100% then include loss = tend in the conditional calculation
+				if (loss <= tend) {
+					if (loss >= tstart) {
+						loss = payout * limit1; //calculate primary payout
+						loss = loss * (1 + scale1); // gross up for secondary coverages
+					}
+					else loss = 0;
+				}
+				else loss = 0;
+			}
+			else { // if the upper threshold is not 100% then do not include loss = tend in the conditional calculation
+				if (loss < tend) {
+					if (loss >= tstart) {
+						loss = payout * limit1; //calculate primary payout
+						loss = loss * (1 + scale1); // gross up for secondary coverages
+					}
+					else loss = 0;
+				}
+				else loss = 0;
+			}
+			if (profile.step_id == 1) {
+				x.step_loss = loss;
+			}
+			else {
+				x.step_loss = x.step_loss + loss;
+			}
+			if (isLast == true) {
+				x.loss = x.step_loss;
+			}
+		}
+		break;
+		case 31:
+		{
+			OASIS_FLOAT tstart = 0;
+			OASIS_FLOAT tend = 0;
+			OASIS_FLOAT payout = 0;
+			OASIS_FLOAT scale1 = 0;
+
+			for (auto y : profile.tc_vec) {
+				if (y.tc_id == trigger_start) tstart = y.tc_val;
+				if (y.tc_id == trigger_end) tend = y.tc_val;
+				if (y.tc_id == payout_start) payout = y.tc_val;
+				if (y.tc_id == scale_1) scale1 = y.tc_val;
+			}
+			// Step policy: single step with fixed monetary payout, no limit, and an optional gross up factor for debris removal
+			OASIS_FLOAT loss = 0;
+
+			loss = x.loss / x.accumulated_tiv;
+			if (tend == 1) { // if the upper threshold is 100% then include loss = tend in the conditional calculation
+				if (loss <= tend) {
+					if (loss >= tstart) {
+						loss = payout; //calculate primary payout
+						loss = loss * (1 + scale1); // gross up for debris removal
+					}
+					else loss = 0;
+				}
+				else loss = 0;
+			}
+			else { // if the upper threshold is not 100% then do not include loss = tend in the conditional calculation
+				if (loss < tend) {
+					if (loss >= tstart) {
+						loss = payout; //calculate primary payout
+						loss = loss * (1 + scale1); // gross up for debris removal
+					}
+					else loss = 0;
+				}
+				else loss = 0;
+			}
+			if (profile.step_id == 1) {
+				x.step_loss = loss;
+			}
+			else {
+				x.step_loss = x.step_loss + loss;
+			}
+			if (isLast == true) {
+				x.loss = x.step_loss;
+			}
+		}
+		break;
 		case 100:	// noop
 		{
 			x.loss = x.loss;
@@ -1021,6 +1119,19 @@ void fmcalc::init_profile__stepped_rec(fm_profile_step& f)
 			add_tc(trigger_end, f.trigger_end, p.tc_vec);
 			break;
 		case 29:
+			add_tc(payout_start, f.payout_start, p.tc_vec);
+			add_tc(scale_1, f.scale1, p.tc_vec);
+			add_tc(trigger_start, f.trigger_start, p.tc_vec);
+			add_tc(trigger_end, f.trigger_end, p.tc_vec);
+			break;
+		case 30:
+			add_tc(payout_start, f.payout_start, p.tc_vec);
+			add_tc(scale_1, f.scale1, p.tc_vec);
+			add_tc(trigger_start, f.trigger_start, p.tc_vec);
+			add_tc(trigger_end, f.trigger_end, p.tc_vec);
+			add_tc(limit_1, f.limit1, p.tc_vec);
+			break;
+		case 31:
 			add_tc(payout_start, f.payout_start, p.tc_vec);
 			add_tc(scale_1, f.scale1, p.tc_vec);
 			add_tc(trigger_start, f.trigger_start, p.tc_vec);

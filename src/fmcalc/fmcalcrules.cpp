b979df94ff817326b7ee95c7331f913f2f292f0e
Joh <johanna.carter@oasislmf.org>
2020-01-10 15:23:22 +0000

    added calcrule 32 for franchise policies and updated 28

--- src/fmcalc/fmcalcrules.cpp
+++ src/fmcalc/fmcalcrules.cpp
@@ -142,7 +142,10 @@ void applycalcrule_stepped(const profile_rec_new& profile, LossRec& x, int layer
 					if (loss >= tstart) {
 						loss = (payout * x.loss) - deductible1; //calculate primary payout
 						if (loss < 0) loss = 0;
-						condloss = loss * scale2; //calculate conditional payout (extra expenses)
+						if (payout == 0) { 
+							condloss = x.step_loss * scale2; //special case to calculate only the conditional coverage loss (extra expenses) based on full input loss where there is no primary payout
+						}
+						else condloss = loss * scale2; //calculate conditional payout based on primary coverage payout (extra expenses)
 						if (condloss > limit2) condloss = limit2; //limit conditional payout
 						loss = loss + condloss; // main coverage + extra expense payout
 						loss = loss * (1 + scale1); // gross up for debris removal
@@ -156,7 +159,10 @@ void applycalcrule_stepped(const profile_rec_new& profile, LossRec& x, int layer
 					if (loss >= tstart) {
 						loss = (payout * x.loss) - deductible1; //calculate primary payout
 						if (loss < 0) loss = 0;
-						condloss = loss * scale2; //calculate conditional payout (extra expenses)
+						if (payout == 0) {
+							condloss = x.step_loss * scale2; //special case to calculate only the conditional coverage loss (extra expenses) based on the step loss brought forward.
+						}
+						else condloss = loss * scale2; //calculate conditional payout (extra expenses)
 						if (condloss > limit2) condloss = limit2; //limit conditional payout
 						loss = loss + condloss; // main coverage + extra expense payout
 						loss = loss * (1 + scale1); // gross up for debris removal
@@ -321,6 +327,41 @@ void applycalcrule_stepped(const profile_rec_new& profile, LossRec& x, int layer
 			if (isLast == true) {
 				x.loss = x.step_loss;
 			}
+		}
+		break;
+		case 32:
+		{
+			OASIS_FLOAT tstart = 0;
+			OASIS_FLOAT payout = 0;
+			OASIS_FLOAT scale1 = 0;
+			OASIS_FLOAT limit1 = 0;
+			
+			for (auto y : profile.tc_vec) {
+				if (y.tc_id == trigger_start) tstart = y.tc_val;
+				if (y.tc_id == payout_start) payout = y.tc_val;
+				if (y.tc_id == scale_1) scale1 = y.tc_val;
+				if (y.tc_id == limit_1) limit1 = y.tc_val;
+			}
+			// Step policy: franchise trigger as a monetary amount with % loss payout, a limit amount and a flat gross up factor for debris removal
+			OASIS_FLOAT loss = 0;
+			if (x.loss >= tstart) {
+				loss = payout * x.loss;
+				if (loss > limit1) loss = limit1;
+				loss = loss * (1 + scale1);
+			}
+			else loss = 0;
+			
+			if (profile.step_id == 1) {
+				x.step_loss = loss;
+			}
+			else {
+				x.step_loss = x.step_loss + loss;
+			}
+			if (isLast == true) {
+				x.loss = x.step_loss;
+			}
+
+
 		}
 		break;
 		case 100:	// noop
@@ -1136,6 +1177,12 @@ void fmcalc::init_profile__stepped_rec(fm_profile_step& f)
 			add_tc(scale_1, f.scale1, p.tc_vec);
 			add_tc(trigger_start, f.trigger_start, p.tc_vec);
 			add_tc(trigger_end, f.trigger_end, p.tc_vec);
+			break;
+		case 32:
+			add_tc(payout_start, f.payout_start, p.tc_vec);
+			add_tc(scale_1, f.scale1, p.tc_vec);
+			add_tc(trigger_start, f.trigger_start, p.tc_vec);
+			add_tc(limit_1, f.limit1, p.tc_vec);
 			break;
 		case 100:
 			break;

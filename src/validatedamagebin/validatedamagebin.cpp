022cb7d83e864688864e427473bccb2bce8e4f53
hchagani-oasislmf <hassan.chagani@oasislmf.org>
2020-11-02 18:02:49 +0000

    Issue deprecation warning if interval_type column present + add check for empty file

--- src/validatedamagebin/validatedamagebin.cpp
+++ src/validatedamagebin/validatedamagebin.cpp
@@ -13,47 +13,111 @@
 
 namespace validatedamagebin {
 
+  inline bool checkfirstbin(float bin_from, int bin_index, char * line) {
+
+    bool dataValid = true;
+
+    // Issue warning if first bin lower limit != 0
+    if(bin_from != 0.0) {
+
+      fprintf(stderr, "Lower limit for first bin is not 0.");
+      fprintf(stderr, " Are you sure this is what you want?:\n");
+      fprintf(stderr, "%s\n", line);
+
+    }
+
+    // First bin index != 1
+    if(bin_index != 1) {
+
+      fprintf(stderr, "First bin index must be 1.\n");
+      fprintf(stderr, "%s\n", line);
+      dataValid = false;
+
+    }
+
+    return dataValid;
+
+  }
+
   void doit() {
 
     damagebindictionary p = {}, q;
     bool dataValid = true;
     char line[4096], prevLine[4096];
+    int lineno = 0;
+    fgets(line, sizeof(line), stdin);   // Skip header
+
+    // Read first line - this will establish format
+    lineno++;
+    bool newFormat;
+    if (fgets(line, sizeof(line), stdin) != 0) {
+      // Deprecated format
+      if (sscanf(line, "%d,%f,%f,%f,%d", &p.bin_index, &p.bin_from, &p.bin_to,
+		 &p.interpolation, &p.interval_type) == 5) {
+
+	fprintf(stderr, "Deprecated format:"
+			" interval_type column no longer required.\n");
+	newFormat = false;
+	dataValid = checkfirstbin(p.bin_from, p.bin_index, line);
+
+      }
+      // New format
+      else if (sscanf(line, "%d,%f,%f,%f", &p.bin_index, &p.bin_from, &p.bin_to,
+		      &p.interpolation) == 4) {
+
+	p.interval_type = 0;
+	newFormat = true;
+	dataValid = checkfirstbin(p.bin_from, p.bin_index, line);
+
+      }
+      // Does not fit either format
+      else {
+
+	fprintf(stderr, "Invalid data in line %d:\n%s\n", lineno, line);
+	dataValid = false;
+
+      }
+    } else {
+      fprintf(stderr, "Empty file\n");
+      exit(EXIT_FAILURE);
+    }
+
     sprintf(prevLine, "%d, %f, %f, %f, %d", p.bin_index, p.bin_from, p.bin_to,
 	    p.interpolation, p.interval_type);
-    int lineno = 0;
-    fgets(line, sizeof(line), stdin);
     lineno++;
 
     while(fgets(line, sizeof(line), stdin) != 0) {
 
-      // Check each line has five values
-      if(sscanf(line, "%d,%f,%f,%f,%d", &q.bin_index, &q.bin_from, &q.bin_to,
-		&q.interpolation, &q.interval_type) != 5) {
+      bool furtherChecks = true;
 
-        fprintf(stderr, "Invalid data in line %d:\n%s\n", lineno, line);
-	dataValid = false;
+      if (sscanf(line, "%d,%f,%f,%f,%d", &q.bin_index, &q.bin_from, &q.bin_to,
+		 &q.interpolation, &q.interval_type) == 5) {
 
-      } else if(lineno == 1) {
-
-	// Issue warning if first bin lower limit != 0
-	if(q.bin_from != 0.0) {
-
-          fprintf(stderr, "Lower limit for first bin is not 0.");
-	  fprintf(stderr, " Are you sure this is what you want?:\n");
-	  fprintf(stderr, "%s\n", line);
-
-	}
-
-	// First bin index != 1
-	if(q.bin_index != 1) {
-
-	  fprintf(stderr, "First bin index must be 1.\n");
-	  fprintf(stderr, "%s\n", line);
+	if (newFormat) {
+	  fprintf(stderr, "Deprecated format used in line %d:\n%s\n",
+		  lineno, line);
 	  dataValid = false;
-
 	}
+
+      } else if (sscanf(line, "%d,%f,%f,%f", &q.bin_index, &q.bin_from,
+			&q.bin_to, &q.interpolation) == 4) {
+
+	if (!newFormat) {
+	  fprintf(stderr, "Missing interval_type column in line %d:\n%s\n",
+		  lineno, line);
+	  dataValid = false;
+	}
+	q.interval_type = 0;
 
       } else {
+
+	fprintf(stderr, "Invalid data in line %d:\n%s\n", lineno, line);
+	dataValid = false;
+	furtherChecks = false;
+
+      }
+
+      if (furtherChecks) {
 
 	// Non-contiguous bin indices
 	if(p.bin_index != q.bin_index-1) {

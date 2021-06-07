308a25f65713101abcb19590d3e8f1ad2aa679ad
hchagani-oasislmf <hassan.chagani@oasislmf.org>
2021-06-07 20:13:59 +0100

    Remove commented line

--- src/include/oasis.h
+++ src/include/oasis.h
@@ -66,7 +66,6 @@ Author: Ben Matharu  email: ben.matharu@oasislmf.org
 // Slowly where applicable we will replace int and OASIS_FLOAT references with
 // OASIS_INT and OASIS_FLOAT so its easy to change the size of the streams 
 typedef int32_t OASIS_INT ;
-//typedef float OASIS_FLOAT;   // HC
 
 // #define OASIS_FLOAT_TYPE_DOUBLE
 #ifdef OASIS_FLOAT_TYPE_DOUBLE
